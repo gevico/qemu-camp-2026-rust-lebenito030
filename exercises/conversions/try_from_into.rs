e1efb9f93bd2422f93dadfdd93b6c1adb61fe248
lebenito030 <i@lebenito.net>
2026-05-18 16:15:59 +0800

    feat(exercise): 完成全部题目

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -23,8 +23,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
 // You need to create an implementation for a tuple of three integers,
@@ -34,10 +32,19 @@ enum IntoColorError {
 // but the slice implementation needs to check the slice length!
 // Also note that correct RGB color values must be integers in the 0..=255 range.
 
+fn validate_component(c: i16) -> Result<u8, IntoColorError> {
+    u8::try_from(c).map_err(|_| IntoColorError::IntConversion)
+}
+
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        Ok(Color {
+            red: validate_component(tuple.0)?,
+            green: validate_component(tuple.1)?,
+            blue: validate_component(tuple.2)?,
+        })
     }
 }
 
@@ -45,6 +52,11 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        Ok(Color {
+            red: validate_component(arr[0])?,
+            green: validate_component(arr[1])?,
+            blue: validate_component(arr[2])?,
+        })
     }
 }
 
@@ -52,6 +64,14 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+        Ok(Color {
+            red: validate_component(slice[0])?,
+            green: validate_component(slice[1])?,
+            blue: validate_component(slice[2])?,
+        })
     }
 }
 
