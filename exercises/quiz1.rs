72fcd31bc2f3979aa79c2aefd6add2a95cb174a1
lebenito030 <i@lebenito.net>
2026-05-18 15:56:43 +0800

    feat(exercise): finish 18/95

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,7 +10,13 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    if quantity > 40 {
+        quantity
+    } else {
+        quantity * 2
+    }
+}
 
 // Put your function here!
 // fn calculate_price_of_apples {
